b63f37786b5db3752438c483970b61c00d60577e
StreetShiffter <agwzdushaz@gmail.com>
2025-09-13 23:51:35 +0700

    Add serializers model 'Course' and 'Lesson'

---
+++ educations/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework.serializers import ModelSerializer
+from educations.models import Course, Lesson
+
+class CourseSerializer(ModelSerializer):
+    """Сериализатор модели 'Курс"""
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+class LessonSerializer(ModelSerializer):
+    """Сериализатор модели 'Урок"""
+    class Meta:
+        model = Lesson
+        fields = '__all__'
