ee1895571cbaf54b57ab022b21ffca86e5fa73df
StreetShiffter <agwzdushaz@gmail.com>
2025-09-19 15:09:09 +0700

    Add new model in app 'Users'

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractUser
 from django.core.validators import RegexValidator
+from django.core.exceptions import ValidationError
+from educations.models import Lesson, Course
 
 
 class CustomUserManager(BaseUserManager):
@@ -63,3 +65,70 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class Payment(models.Model):
+    """Модель оплаты куросов или уроков"""
+    PAYMENT_METHOD_CHOICES = [
+        ('cash', 'Наличные'),
+        ('transfer', 'Перевод на счет'),
+    ]
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name="Пользователь",
+        related_name="payments"
+    )
+    payment_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата оплаты"
+    )
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        verbose_name="Оплаченный курс",
+        related_name="payments"
+    )
+    lesson = models.ForeignKey(
+        Lesson,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        verbose_name="Оплаченный урок",
+        related_name="payments"
+    )
+    #DecimalField - правильная работа только для денег
+    amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name="Сумма оплаты"
+    )
+    payment_method = models.CharField(
+        max_length=10,
+        choices=PAYMENT_METHOD_CHOICES,
+        verbose_name="Способ оплаты"
+    )
+
+    class Meta:
+        verbose_name = "Платеж"
+        verbose_name_plural = "Платежи"
+        ordering = ['-payment_date']
+
+    def __str__(self):
+        """Выбор названия курса или урока на выбор"""
+        target = self.course.title if self.course else (self.lesson.title if self.lesson else "Не указано")
+        return f"Платеж {self.amount} от {self.user} за {target}"
+
+    def clean(self):
+        """Проверка оплаты - валидация"""
+        if not self.course and not self.lesson:
+            raise ValidationError("Должен быть указан хотя бы один: курс или урок.")
+        if self.course and self.lesson:
+            raise ValidationError("Нельзя одновременно указать и курс, и урок. Выберите что-то одно.")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()  # вызываем валидацию перед сохранением
+        super().save(*args, **kwargs)
