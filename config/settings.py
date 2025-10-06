fb091a77712a11e33bcf80860c18d4756e79b6b9
StreetShiffter <agwzdushaz@gmail.com>
2025-10-06 12:34:48 +0700

    добавлен cors для будущего фронтенда

--- config/settings.py
+++ config/settings.py
@@ -34,7 +34,8 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     "users",
     "django_filters",
-    "drf_spectacular"
+    "drf_spectacular",
+    # "corsheaders",
 ]
 
 REST_FRAMEWORK = {
@@ -60,7 +61,19 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    # "corsheaders.middleware.CorsMiddleware",
 ]
+
+# CORS_ALLOWED_ORIGINS = [
+#     '<http://localhost:8000>',  # Замените на адрес вашего фронтенд-сервера
+# ]
+#
+# CSRF_TRUSTED_ORIGINS = [
+#     "https://read-and-write.example.com", #  Замените на адрес вашего фронтенд-сервера
+#     # и добавьте адрес бэкенд-сервера
+# ]
+#
+# CORS_ALLOW_ALL_ORIGINS = False
 
 ROOT_URLCONF = "config.urls"
 
