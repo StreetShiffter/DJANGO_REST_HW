16a444cd6b993f644ab6525f2db938e56d6a8ca1
StreetShiffter <agwzdushaz@gmail.com>
2025-10-28 15:59:44 +0700

    test templates fix4

--- config/settings.py
+++ config/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.getenv("DEBUG") == "True" else False
 # * только для разработки\ 'localhost', '127.0.0.1' -для прода
-ALLOWED_HOSTS = [os.getenv("ALLOWED_HOSTS", "*")]
+ALLOWED_HOSTS = ["*"]
 
 
 
