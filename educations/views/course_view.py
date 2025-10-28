d436994a05a52366db9888373b1019a3075bbc72
StreetShiffter <agwzdushaz@gmail.com>
2025-10-28 16:27:54 +0700

    test templates fix6

--- educations/views/course_view.py
+++ educations/views/course_view.py
@@ -11,7 +11,7 @@ from users.permissions import IsOwnerOrModerator
 
 
 class HomeView(TemplateView):
-    template_name = 'home.html'
+    template_name = 'educations/home.html'
 
 class CourseViewSet(viewsets.ModelViewSet):
     """ViewSet-класс для вывода списка курсов и информации по одному объекту
