e3acd4ed995d8b5a4028af21d13ce7fb0706eace
StreetShiffter <agwzdushaz@gmail.com>
2025-10-29 18:04:34 +0700

    Test CI_4

--- config/urls.py
+++ config/urls.py
@@ -23,9 +23,15 @@ urlpatterns = [
     ),
 ]
 
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#     urlpatterns += static(
+#         settings.STATIC_URL,
+#         document_root=settings.STATIC_ROOT or settings.STATICFILES_DIRS[0],
+#     )
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(
         settings.STATIC_URL,
-        document_root=settings.STATIC_ROOT or settings.STATICFILES_DIRS[0],
+        document_root=settings.STATIC_ROOT
+        or (settings.STATICFILES_DIRS[0] if settings.STATICFILES_DIRS else None),
     )
